5095e5be7560e77fc654f3897643176edc8f1bbb
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-27 18:37:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,12 +38,7 @@ class Song
   end
   
   def self.find_or_create_by_name(str_name)
-    if str_name == find_by_name(str_name)
-      return str_name 
-    else
-    create_by_name(str_name)
-    end
-    str_name
+   self.str_name == find_by_name(str_name) || self.create_by_name(str_name)
   end
   
   def self.alphabetical
