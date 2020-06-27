cde072985269dd3f5a7315a7b0969b72ed883cab
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-27 18:36:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,11 +51,11 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+    sorted_file_name = file_name.split(" - ").delete(".mp3")
+    artist_name = sorted_file_name[0]
+    song_name = sorted_file_name[1]
 
-    song = self.new
+    song = self.create
     song.name = song_name
     song.artist_name = artist_name
     song
